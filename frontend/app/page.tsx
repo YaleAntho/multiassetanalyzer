c3c13e3c52452aaf098f5ae928e89fc86db455ed
HughBlume <meratideocampo@gmail.com>
2025-11-07 11:15:09 +0800

    Add landing page

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { MultiAssetAnalyzerDemo } from "@/components/MultiAssetAnalyzerDemo";
+
+export default function Home() {
+  return (
+    <main className="w-full">
+      <MultiAssetAnalyzerDemo />
+    </main>
+  );
+}
+
